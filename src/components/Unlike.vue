49abc7766a5bbac1bdb599979003cfd0f4bf6113
Trần Ngọc Minh <ngocminh2306@gmail.com>
2018-08-06 09:33:10 +0700

    cm

---
+++ src/components/Unlike.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h3>Không thích !!</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "unlike",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/like.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h2>
+      like page
+    </h2>
+    <input placeholder="Mã đăng nhập"/>
+    <router-link class="btn-primary" to="/content">
+        <button>Đăng nhập</button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hello",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
+}
+</style>
