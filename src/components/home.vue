ddb0db1a94074c045fe01221a8027ec19337aa90
Tran ngoc minh <ngocminh2306@gmail.com>
2018-08-09 00:13:45 +0700

    cm

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="hello">
-     <div id="demo">
-      <transition class="show2" name="slide-fade-out">
+    <Snowf
+  :amount="100"
+  :size="8"
+  :speed="2.5"
+  :wind="0"
+  :opacity="1"
+  :swing="1"
+  :image="null"
+  :zIndex="null"
+  :resize="true"
+  color="#fff"
+/>
+     <div id="thao">
+      <!-- <transition class="show2" name="slide-fade-out">
         <img style="float: left; height:250px; width: 200px; margin-left: 100px;" v-if="show1" src="../assets/thao1.png">
       </transition>
       <transition class="show2" name="slide-fade">
         <img style="float: right; height:250px; width: 200px; margin-right: 100px;" v-if="show1" src="../assets/thao3.png">
-      </transition>
-
+      </transition> -->
+        <img id="imgthao1" style="left:0px; height:250px; width: 200px; margin-right: 100px;" src="../assets/thao3.png">
+        <img id="imgthao2" style="left:20%; height:100px; width: 100px;" src="../assets/thao3.png">
+        <img id="imgthao3" style="left:30%; height:100px; width: 100px;" src="../assets/thao3.png">
+        <img id="imgthao4" style="left:50%; height:100px; width: 100px;" src="../assets/thao3.png">
+        <img id="imgthao5" style="left:60%; height:100px; width: 100px;" src="../assets/thao3.png">
+        <img id="imgthao6" style="left:40%; height:100px; width: 100px;" src="../assets/thao3.png">
+        <img id="imgthao7" style="left:500px; height:100px; width: 100px;" src="../assets/thao3.png">
+        <img id="imgthao8" style="left:95%; height:100px; width: 100px;" src="../assets/thao3.png">
+        <img id="imgthao9" style="left:80%; height:100px; width: 100px;" src="../assets/thao3.png">
+        <img id="imgthao10" style="left:70%; height:100px; width: 100px;" src="../assets/thao3.png">
       <ul>
         <h2>Thích trang web này không em?</h2>
         <li>
@@ -29,12 +50,17 @@
 </template>
 
 <script>
+import Snowf from "vue-snowf";
 export default {
+  components: {
+    Snowf
+  },
   name: "home",
   data() {
     return {
       show: false,
-      show1: false
+      show1: false,
+      snow: "../assets/thao1.png"
     };
   },
   mounted() {
@@ -91,5 +117,87 @@ a {
 .slide-fade-leave-to {
   transform: translateX(-300px);
   opacity: 1;
+}
+
+
+/*icon move top to bottom*/
+
+#imgthao1 {
+  animation: flow1 10s infinite;
+  animation-direction: normal ;
+  position: absolute; 
+}
+#imgthao2 {
+  animation: flow1 7s infinite;
+  animation-direction: normal ;
+  position: absolute; 
+}
+#imgthao3 {
+  animation: flow1 5s infinite;
+  animation-direction: normal ;
+  position: absolute; 
+}
+#imgthao3 {
+  animation: flow2 10s infinite;
+  animation-direction: normal ;
+  position: absolute; 
+}
+#imgthao4 {
+  animation: flow1 5s infinite;
+  animation-direction: normal ;
+  position: absolute; 
+}
+#imgthao5 {
+  animation: flow1 10s infinite;
+  animation-direction: normal ;
+  position: absolute; 
+}
+#imgthao6 {
+  animation: flow1 10s infinite;
+  animation-direction: normal ;
+  position: absolute; 
+}
+#imgthao7 {
+  animation: flow3 3s infinite;
+  animation-direction: normal ;
+  position: absolute; 
+}
+#imgthao8 {
+  animation: flow2 20s infinite;
+  animation-direction: normal ;
+  position: absolute; 
+}
+#imgthao9 {
+  animation: flow1 5s infinite;
+  animation-direction: normal ;
+  position: absolute; 
+}#imgthao10 {
+  animation: flow1 9s infinite;
+  animation-direction: normal ;
+  position: absolute; 
+}
+@keyframes flow1 {
+    from{ top: -50px;
+     transform: rotate(-90deg);
+     }
+    to{ top: 100%;
+    transform: rotate(270deg)
+    }
+}
+@keyframes flow2 {
+    from{ top: -50px;
+     transform: rotate(90deg);
+     }
+    to{ top: 100%;
+    transform: rotate(-360deg)
+    }
+}
+@keyframes flow3 {
+    from{ top: -50px;
+     transform: rotate(-80deg);
+     }
+    to{ top: 100%;
+    transform: rotate(200deg)
+    }
 }
 </style>
